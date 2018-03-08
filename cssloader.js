db08a6531496ccb9cad031d0f34c8920f95c58e0
Einar Norðfjörð <einar@vortex.is>
2018-03-08 23:13:30 +0000

    initial state for the cssloader demo

--- cssloader.js
+++ cssloader.js
@@ -1,4 +1,4 @@
-const createStyleSheet = href => {
+const loadCSS = href => {
   const link = document.createElement('link')
   link.rel = 'stylesheet'
 
@@ -9,18 +9,7 @@ const createStyleSheet = href => {
   return link
 }
 
-const createStyleSheet = href => {
-  const link = document.createElement('link')
-  link.rel = 'stylesheet'
-
-  link.href = href
-
-  document.head.append(link)
-
-  return link
-}
-
-const loadCSS = cssPath => createStyleSheet(`./styles/_default/main.css`)
+loadCSS('./styles/_default/main.css')
 
 document.querySelectorAll('.theme-selector').forEach(el => {
   el.onclick = () => {
